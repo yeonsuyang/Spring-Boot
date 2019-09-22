9b7c653e68e8e273570431e2144e421acdb5a417
yeonsuyang <ys.yang@gmail.com>
2019-09-22 13:28:23 +0900

    sbmarket

---
+++ marketProject/src/test/java/com/ysyang/sbmarket/SbmarketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ysyang.sbproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbmarketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
